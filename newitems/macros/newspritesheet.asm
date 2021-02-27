4d66d5e5ac00f487b7cf986566473def61c2b12d
phi <ficarde@gmail.com>
2021-02-27 20:09:47 -0300

    Make Sprite Sheet system more dynamic  This adds a SpriteMemory pseudo-datatype that tracks the memory addresses of the sprites as they are included and automatically increments them, allowing us to avoid having to set the starting address of every sprite.

--- newitems/macros/newspritesheet.asm
+++ newitems/macros/newspritesheet.asm
@@ -1,31 +1,46 @@
 !SpriteSheetId = 0
+!SpriteMemoryId = 0
 
-macro NewSpriteSheet(var, spritetype, file, address, add_to_sheet_library)
-    !{<var>_address} #= <address>
-    !{<var>_bank} #= bank(<address>)
-    !{<var>_word} #= word(<address>)
-    !{<var>_high} #= hibyte(<address>)
-    !{<var>_mid}  #= midbyte(<address>)
-    !{<var>_low}  #= lowbyte(<address>)
+macro NewSpriteMemory(var, starting_address, end_address)
+    !<var> #= !SpriteMemoryId
+    !{SpriteMemory!{SpriteMemoryId}_starting_address} #= <starting_address>
+    !{SpriteMemory!{SpriteMemoryId}_end_address} #= <end_address>
+    !{SpriteMemory!{SpriteMemoryId}_address} #= <starting_address>
+    !SpriteMemoryId #= !SpriteMemoryId+1
+endmacro
+
+macro NewSpriteSheet(var, spritetype, file, memory, add_to_sheet_library)
+    !memory #= <memory>
+    !address #= !{SpriteMemory!{memory}_address}
+    !{<var>_address} #= !address
+    !{<var>_bank} #= bank(!address)
+    !{<var>_word} #= word(!address)
+    !{<var>_high} #= hibyte(!address)
+    !{<var>_mid}  #= midbyte(!address)
+    !{<var>_low}  #= lowbyte(!address)
     if <add_to_sheet_library> != 0
         !{<var>_id}   #= !SpriteSheetId
-        !{SpriteSheet!{SpriteSheetId}_high} #= hibyte(<address>)
-        !{SpriteSheet!{SpriteSheetId}_mid}  #= midbyte(<address>)
-        !{SpriteSheet!{SpriteSheetId}_low}  #= lowbyte(<address>)
+        !{SpriteSheet!{SpriteSheetId}_high} #= hibyte(!address)
+        !{SpriteSheet!{SpriteSheetId}_mid}  #= midbyte(!address)
+        !{SpriteSheet!{SpriteSheetId}_low}  #= lowbyte(!address)
         !SpriteSheetId #= !SpriteSheetId+1
     endif
     pushpc
-    org <address>
+    org !address
         incbin newitems/sprites/<spritetype>/<file>
         if stringsequal("<spritetype>", "2bpp")
-            warnpc <address>+$0400
+            warnpc !address+$0400
+            !{SpriteMemory!{memory}_address} #= !address+$0400
         elseif stringsequal("<spritetype>", "3bpp")
-            warnpc <address>+$0600
+            warnpc !address+$0600
+            !{SpriteMemory!{memory}_address} #= !address+$0600
         elseif stringsequal("<spritetype>", "4bpp")
-            warnpc <address>+$0800
+            warnpc !address+$0800
+            !{SpriteMemory!{memory}_address} #= !address+$0800
         else
             error "Unknown type for sprite: '<spritetype>'"
         endif
+    warnpc !{SpriteMemory!{memory}_end_address}
     pullpc
 endmacro
 
--- newitems/sprites/main.asm
+++ newitems/sprites/main.asm
@@ -1,12 +1,10 @@
-!NewItemSpriteBank = $A5
-
-function new_item_sprite_address(offset) = (!NewItemSpriteBank<<16)+$8000+offset
+%NewSpriteMemory(NewItemSpriteMemory, $A58000, $A60000)
 
 if !CompressMenuRingsGFX != 0
-    %NewSpriteSheet(GfxMenuRings, 2bpp, menurings.gfx, new_item_sprite_address($0000), 0)
+    %NewSpriteSheet(GfxMenuRings, 2bpp, menurings.gfx, !NewItemSpriteMemory, 0)
 else
-    %NewSpriteSheet(GfxMenuRings, 2bpp, menurings.bin, new_item_sprite_address($0000), 0)
+    %NewSpriteSheet(GfxMenuRings, 2bpp, menurings.bin, !NewItemSpriteMemory, 0)
 endif
 
-%NewSpriteSheet(GfxRings, 3bpp, rings.gfx, new_item_sprite_address($0400), 1)
+%NewSpriteSheet(GfxRings, 3bpp, rings.gfx, !NewItemSpriteMemory, 1)
 
